8cdfe63bbde6f12ee650ffc7918610f253ba4750
Jun Xian Yong <junxian.yong@gmail.com>
2024-07-15 13:13:22 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v $WORKSPACE/src:/var/www/html php:7.2-apache
-sleep 1
+docker network inspect jenkins_network >/dev/null 2>&1 || docker network create jenkins_network
+docker run -d --name my-apache-php-app --network jenkins_network -p 80:80 -v $WORKSPACE/src:/var/www/html php:7.2-apache
+sleep 5
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://my-apache-php-app to see your PHP application in action.'
